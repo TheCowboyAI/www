02dfd10cb65885df9884120291344549c16244fe
Steele Price <steele@thecowboy.ai>
2024-01-19 10:51:12 -0700

    style and content tweaks

--- src/main.rs
+++ src/main.rs
@@ -22,27 +22,73 @@ fn App() -> impl IntoView {
 fn Content() -> impl IntoView {
     view! {
         <div class="ml-5 md:ml-24">
-            <h1 class="text-4xl md:text-5xl m-2.5 caption text-shadow-lg shadow-black">{"AI-Driven Business Transformation"}</h1>
-            <div class="shadow-md rounded-md m-2.5">
-                <p class="mr-0 lg:mr-20 text-lg text-shadow-lg shadow-black text-white">
-                {"Imagine a world where every facet of your business is not just enhanced, 
-                but transformed by artificial intelligence. That's the world Cowboy AI is 
-                creating. Our platform isn't just a service; it's a partnership, where AI 
-                solutions are not just applied but are intricately tailored to you. 
-                Forget the one-size-fits-all approach. Here, AI becomes a seamless 
-                extension of your strategy, whether it's through automating mundane tasks, 
-                deriving profound insights from your data, or engaging with your customers 
-                on a whole new level. With our robust suite of AI tools and APIs, 
-                the power of machine learning, natural language processing, and 
-                predictive analytics is at your fingertips. Backed by strategic 
-                partnerships with tech titans and a strong commitment to open-source 
-                innovation, Cowboy AI is not just your provider but your co-pilot in the 
-                journey toward digital excellence. Embrace the future where every click, 
-                every decision, and every interaction is empowered by AI."}
-                </p>
-                <p class="font-bold mt-10 mr-0 lg:mr-20 text-xl text-shadow-lg shadow-black text-white">
-                {"With Cowboy AI, it's not just business as usual; it's business ahead of the curve."}
-                </p>
+            <h1 class="text-4xl md:text-5xl m-2.5 caption bg-black shadow-lg text-center rounded-lg">
+            {"AI-Driven Business Transformation"}
+            </h1>
+            <div class="rounded-md m-2.5">
+                <div class="lg:mr-20 text-white">
+
+                    <h2 class="text-2xl md:text-4xl my-2.5 caption">
+                    {"Imagine..."}
+                    </h2>
+
+                    <p class="text-lg text-shadow-lg shadow-black">
+                    {"a world where every facet of your business is not just enhanced,
+                    but transformed with insight through artificial intelligence."}
+                    </p>
+
+                    <blockquote class="italic text-lg text-white-300 mt-5 pl-10">
+                    {"- That's the world Cowboy AI is creating. -"}
+                    </blockquote>
+
+                    <h2 class="text-2xl md:text-4xl my-2.5 caption">
+                    {"Your platform as a partnership"}
+                    </h2>
+
+                    <p class="lg:mr-20 text-lg text-shadow-lg shadow-black text-white">
+                    {"AI solutions are not just applied but intricately tailored to you.
+                    Forget the one-size-fits-all approach, you are special."}
+                    </p>
+
+                    <h2 class="text-2xl md:text-4xl my-2.5 caption">
+                    {"Strategic extensions"}
+                    </h2>
+
+                    <ul>
+                        <li>{"Automate mundane tasks"}</li>
+                        <li>{"Understand workflows"}</li>
+                        <li>{"Derive profound insights from your data"}</li>
+                        <li>{"Engaging with your customers on a whole new level."}</li>
+                    </ul>
+
+                    <h2 class="text-2xl md:text-4xl my-2.5 caption">
+                    {"AI tools and APIs"}
+                    </h2>
+
+                    <ul>
+                        <li>{"Any LLM"}</li>
+                        <li>{"Machine Learning"}</li>
+                        <li>{"Natural Language Processing"}</li>
+                        <li>{"Voice Recognition"}</li>
+                        <li>{"Predictive Analytics"}</li>
+                    </ul>
+
+                    <h2 class="text-2xl md:text-4xl my-2.5 caption">
+                    {"Strategic partnerships"}
+                    </h2>
+                    <p class="text-shadow-lg shadow-black text-white">
+                    {"We work with the tech titans and navigate open-source solutions to fit your model.
+                    Cowboy AI is not just your provider but your co-pilot in the 
+                    journey toward digital excellence."}
+                    </p>
+
+                    <p class="font-bold mt-10 text-xl text-shadow-lg shadow-black text-white">
+                    {"With Cowboy AI, it's not just business as usual;"}
+                    </p>
+                    <p class="font-bold text-xl text-shadow-lg shadow-black text-white">
+                    {"it's business ahead of the curve."}
+                    </p>
+                </div>
             </div>
         </div>
     }
@@ -62,26 +108,25 @@ fn SvgImage() -> impl IntoView {
 
 use leptos::*;
 
-#[component]
-fn DashboardIcon() -> impl IntoView {
-    let (content, set_content) = create_signal(String::from("Initial content"));
+// #[component]
+// fn DashboardIcon() -> impl IntoView {
+//     let (content, set_content) = create_signal(String::from("Initial content"));
 
-    let handle_click = move |_| {
-        set_content(String::from("New content after click"));
-    };
+//     let handle_click = move |_| {
+//         set_content(String::from("New content after click"));
+//     };
 
-    view! {
-        div {
-            "Content: "
-            {content()}
-            button {
-                on:click=handle_click,
-                "Change Content"
-            }
-        }
-    }
-}
-
+//     view! {
+//         div {
+//             "Content: "
+//             {content()}
+//             button {
+//                 on:click=handle_click,
+//                 "Change Content"
+//             }
+//         }
+//     }
+// }
 
 #[component]
 fn Dashboard() -> impl IntoView {
@@ -92,7 +137,7 @@ fn Dashboard() -> impl IntoView {
                 class="mx-auto w-20 h-20"
             />
         </div>
-    
+
     }
 }
 
@@ -100,44 +145,29 @@ fn Dashboard() -> impl IntoView {
 fn Navigate() -> impl IntoView {
     view! {
         // mini menu
-        <div class="icon md:invisible">
-            <div class="svg-container">
+        <div>
+    <nav class="flex flex-col w-48 h-screen px-4 border border-black rounded-md">
+        <div class="mt-3">
+            <div class="icon ml-10 px-4 py-4 flex flex-row svg-container items-center">
                 <img src="assets/logo.svg" class="mx-auto" />
+                <div class="font-bold caption text-center mb-4">Cowboy AI</div>
             </div>
-        </div>
-        // full menu
-        <div class="inset-y-0 left-0 hidden md:block">
-        <nav class="flex flex-col cowboy-blue w-48 h-screen px-4 border border-black rounded-md">
-          <div class="flex flex-wrap mt-8 bg-white rounded-lg items-center p-3">
-            <div class="w-1/2">
-              <img
-                src="assets/logo.svg"
-                class="mx-auto w-20 h-20"
-              />
-            </div>
-            <div class="w-1/2">
-              <span class="font-bold caption">Cowboy AI</span>
-            </div>
-          </div>
-          <div class="mt-10 mb-4">
+            <div class="mt-10 mb-4">
             <ul class="ml-4">
-            <li class="mb-2 px-4 py-4 text-gray-100 flex flex-row border-black hover:text-black hover:bg-white hover:font-bold rounded rounded-lg">
-            <span>
-                  <svg class="fill-current h-5 w-5 " viewBox="0 0 24 24">
-                    <path
-                      d="M16 20h4v-4h-4m0-2h4v-4h-4m-6-2h4V4h-4m6
-                          4h4V4h-4m-6 10h4v-4h-4m-6 4h4v-4H4m0 10h4v-4H4m6
-                          4h4v-4h-4M4 8h4V4H4v4z"
-                    ></path>
-                  </svg>
-                </span>
-                <a href="#">
-                  <span class="ml-2">Dashboard</span>
-                </a>
-              </li>
-              <li class="mb-2 px-4 py-4 text-gray-100 flex flex-row border-black hover:text-black hover:bg-white hover:font-bold rounded rounded-lg">
+            <li class="mb-2 px-4 py-4 flex flex-row">
+            <a href="#" class="border-black hover:text-black hover:bg-white hover:font-bold rounded">
+              <svg class="fill-current h-5 w-5 " viewBox="0 0 24 24">
+                <title>Dashboard</title>
+                <path d="M16 20h4v-4h-4m0-2h4v-4h-4m-6-2h4V4h-4m6 4h4V4h-4m-6 10h4v-4h-4m-6 4h4v-4H4m0 10h4v-4H4m6 4h4v-4h-4M4 8h4V4H4v4z"/>
+              </svg>
+            </a>
+            </li>
+
+             <li class="mb-2 px-4 py-4 flex flex-row">
+              <a href="#" class="border-black hover:text-black hover:bg-white hover:font-bold rounded">
                 <span>
                   <svg class="fill-current h-5 w-5 " viewBox="0 0 24 24">
+                  <title>Articles</title>
                     <path
                       d="M12 13H7v5h5v2H5V10h2v1h5v2M8
                           4v2H4V4h4m2-2H2v6h8V2m10 9v2h-4v-2h4m2-2h-8v6h8V9m-2
@@ -145,11 +175,11 @@ fn Navigate() -> impl IntoView {
                     ></path>
                   </svg>
                 </span>
-                <a href="#">
-                  <span class="ml-2">Articles</span>
                 </a>
               </li>
-              <li class="mb-2 px-4 py-4 text-gray-100 flex flex-row border-black hover:text-black hover:bg-white hover:font-bold rounded rounded-lg">
+
+              <li class="mb-2 px-4 py-4 flex flex-row">
+              <a href="#" class="border-black hover:text-black hover:bg-white hover:font-bold rounded">
                 <span>
                   <svg
                     class="fill-current h-5 w-5 "
@@ -157,6 +187,7 @@ fn Navigate() -> impl IntoView {
                     fill="none"
                     xmlns="http://www.w3.org/2000/svg"
                   >
+                    <title>Customers</title>
                     <path
                       fill-rule="evenodd"
                       clip-rule="evenodd"
@@ -169,41 +200,40 @@ fn Navigate() -> impl IntoView {
                     />
                   </svg>
                 </span>
-                <a href="#">                 
-                  <span class="ml-2">Customers</span>
                 </a>
               </li>
-              <li class="mb-2 px-4 py-4 text-gray-100 flex flex-row border-black hover:text-black hover:bg-white hover:font-bold rounded rounded-lg">
-                <span>
-                  <svg class="fill-current h-5 w-5 " viewBox="0 0 24 24">
-                    <path
-                      d="M19 19H5V8h14m-3-7v2H8V1H6v2H5c-1.11 0-2 .89-2
-                          2v14a2 2 0 002 2h14a2 2 0 002-2V5a2 2 0
-                          00-2-2h-1V1m-1 11h-5v5h5v-5z"
-                    ></path>
-                  </svg>
-                </span>
-                <a href="#">
-                 
-                  <span class="ml-2">Milestones</span>
+
+              <li class="mb-2 px-4 py-4 flex flex-row">
+              <a href="#" class="border-black hover:text-black hover:bg-white hover:font-bold rounded">
+                      <span>
+                        <svg class="fill-current h-5 w-5 " viewBox="0 0 24 24">
+                            <title>Milestones</title>
+                            <path
+                                d="M19 19H5V8h14m-3-7v2H8V1H6v2H5c-1.11 0-2 .89-2
+                                2v14a2 2 0 002 2h14a2 2 0 002-2V5a2 2 0
+                                00-2-2h-1V1m-1 11h-5v5h5v-5z" />
+                        </svg>
+                    </span>
                 </a>
               </li>
-              <li class="mb-2 px-4 py-4 text-gray-100 flex flex-row border-black hover:text-black hover:bg-white hover:font-bold rounded rounded-lg">
-                <span>
-                  <svg class="fill-current h-5 w-5" viewBox="0 0 24 24">
-                    <path
-                      d="M12 4a4 4 0 014 4 4 4 0 01-4 4 4 4 0 01-4-4 4 4 0
-                          014-4m0 10c4.42 0 8 1.79 8 4v2H4v-2c0-2.21 3.58-4
-                          8-4z"
-                    ></path>
-                  </svg>
-                </span>
-                <a href="#">
-                  <span class="ml-2">Team</span>
+
+              <li class="mb-2 px-4 py-4 flex flex-row">
+              <a href="#" class="border-black hover:text-black hover:bg-white hover:font-bold rounded">
+                  <span>
+                    <svg class="fill-current h-5 w-5" viewBox="0 0 24 24">
+                    <title>Team</title>
+                        <path
+                        d="M12 4a4 4 0 014 4 4 4 0 01-4 4 4 4 0 01-4-4 4 4 0
+                            014-4m0 10c4.42 0 8 1.79 8 4v2H4v-2c0-2.21 3.58-4
+                            8-4z"
+                        ></path>
+                    </svg>
+                    </span>
                 </a>
               </li>
             </ul>
           </div>
+        </div>
         </nav>
       </div>
       }
