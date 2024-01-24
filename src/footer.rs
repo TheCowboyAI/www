5be794bd571b8ce55d5db89bcce19826ceb9efa0
Steele Price <steele@thecowboy.ai>
2024-01-24 16:50:31 -0700

    fade copyright

--- src/footer.rs
+++ src/footer.rs
@@ -3,7 +3,7 @@ use leptos::*;
 #[component]
 pub fn Footer() -> impl IntoView {
     view! {
-        <div class="rounded-lg leading-6 p-2 text-white bg-black">
+        <div class="rounded-lg leading-6 p-2 text-white bg-black opacity-50">
             <p class="text-sm">Copyright 2024</p>
             <p class="text-sm">Cowboy AI</p>
         </div>
