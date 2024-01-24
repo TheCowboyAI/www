c0e2d86cb926dc63ecafb925bcf6571e5e7dab58
Steele Price <steele@thecowboy.ai>
2024-01-24 16:56:17 -0700

    fix mobile

--- src/customwork.rs
+++ src/customwork.rs
@@ -5,29 +5,29 @@ pub fn CustomWork() -> impl IntoView {
     view! {
         <div class="flex items-center animate-fadeIn">
             <div class="flex-row">
-                <h2 class="font-bold text-white text-4xl md:text-6xl">
+                <h2 class="font-bold text-white text-2xl md:text-4xl lg:text-6xl">
                     {"Custom Work"}
                 </h2>
-                <h3 class="text-3xl md:text-4xl mt-12 font-bold">
+                <h3 class="text-xl md:text-2xl lg:text-4xl mt-12 font-bold">
                 {"We offer several custom solutions"}
                 </h3>
                 <ul class="border-dashed text-base mt-10 px-5 py-2 text-white">
-                    <li class="text-xl font-bold">{"Custom GPTs"}</li>
+                    <li class="text-base lg:text-xl font-bold">{"Custom GPTs"}</li>
                     <li class="ml-2 text-black">{"Let us build a GPT just for you"}</li>
                     
-                    <li class="text-xl font-bold">{"Business Model Workshop"}</li>
+                    <li class="text-base lg:text-xl font-bold">{"Business Model Workshop"}</li>
                     <li class="ml-2 text-black">{"Workshop to help you understand and generate a Business Model Canvas"}</li>
                     
-                    <li class="text-xl font-bold">{"Business Knowledge Workshop"}</li>
+                    <li class="text-base lg:text-xl font-bold">{"Business Knowledge Workshop"}</li>
                     <li class="ml-2 text-black">{"Understand how Taxonomies, Ontologies and Knowledge Graphs help you leverage AI"}</li>
                     
-                    <li class="text-xl font-bold">{"Custom Development"}</li>
+                    <li class="text-base lg:text-xl font-bold">{"Custom Development"}</li>
                     <li class="ml-2 text-black">{"We build systems in Leptos, a cutting-edge Rust framework for the modern web"}</li>
                     
-                    <li class="text-xl font-bold">{"NixOS Work"}</li>
+                    <li class="text-base lg:text-xl font-bold">{"NixOS Work"}</li>
                     <li class="ml-2 text-black">{"Build determinate systems and deploy them anywhere"}</li>
                     
-                    <li class="text-xl font-bold">{"Event Sourcing"}</li>
+                    <li class="text-base lg:text-xl font-bold">{"Event Sourcing"}</li>
                     <li class="ml-2 text-black">{"Learn, Create and Use Event Sourced Systems"}</li>
                 </ul>
                 <form
