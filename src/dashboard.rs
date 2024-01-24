9b26521cd5566db116fea477a07522f061267895
Steele Price <steele@thecowboy.ai>
2024-01-24 13:46:54 -0700

    add content

--- src/dashboard.rs
+++ src/dashboard.rs
@@ -3,11 +3,21 @@ use leptos::*;
 #[component]
 pub fn Dashboard() -> impl IntoView {
     view! {
-        <div class="icon">
-            <img
-                src="assets/logo.svg"
-                class="mx-auto w-20 h-20"
-            />
+        <div class="h-screen flex items-center animate-fadeIn">
+            <div class="flex-row"> 
+                <h1 class="font-bold text-white text-4xl md:text-6xl">
+                {"Dashboards over Reports"}
+                </h1>
+                <div class="text-black flex-row mt-10">
+                    <ul class="text-2xl">
+                    <li>{"Real-Time Monitoring"}</li>
+                    <li>{"Push to Mobile"}</li>
+                    <li>{"Improved Decision-Making"}</li>
+                    <li>{"Enhanced Communication"}</li>
+                    <li>{"Time and Resource Efficiency"}</li>
+                    </ul>
+                </div>
+            </div>
         </div>
 
     }
--- src/people.rs
+++ src/people.rs
@@ -4,11 +4,20 @@ use leptos::*;
 pub fn People() -> impl IntoView {
     view! {
         <div class="h-screen flex items-center animate-fadeIn">
-            <img
-                src="assets/logo.svg"
-                class="mx-auto w-20 h-20"
-            />
+            <div class="flex-row"> 
+                <h1 class="font-bold text-white text-4xl md:text-6xl">
+                {"People are the Cornerstone"}
+                </h1>
+                <div class="text-black flex-row mt-10">
+                    <ul class="text-2xl">
+                    <li>{"Secure Everything"}</li>
+                    <li>{"Communicate Efficiently"}</li>
+                    <li>{"Visualize Relationships"}</li>
+                    <li>{"Collaborate with Ease"}</li>
+                    <li>{"Use a Common Language"}</li>
+                    </ul>
+                </div>
+            </div>
         </div>
-
     }
 }
