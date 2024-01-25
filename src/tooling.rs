abae7bd5062cfe54789d0133bca0c098035bf244
Steele Price <steele@thecowboy.ai>
2024-01-25 16:39:01 -0700

    add button

--- src/tooling.rs
+++ src/tooling.rs
@@ -19,16 +19,35 @@ pub fn Tooling() -> impl IntoView {
                     {"AI Composable Insights"}
                     </h2>
                 </div>
-                <a
-                  class="hover:cursor-hand"
-                  href="/gpts"
-                >
-                    <h2 class="mt-10 font-bold text-white text-3xl md:text-4xl">
-                    {"Our Custom GPTs"}
-                    <br />
-                    {"Fit Your Model"}
-                    </h2>
-                </a>
+
+                <h2 class="mt-10 font-bold text-white text-3xl md:text-4xl">
+                {"Custom GPTs"}
+                <br />
+                {"Fit Your Model"}
+                </h2>
+                <div class="mt-10">
+                    <a  
+                    href="/gpts"
+                    class="
+                        text-white 
+                        bg-gradient-to-br 
+                        from-purple-600 
+                        to-blue-500 
+                        hover:bg-gradient-to-bl 
+                        focus:ring-4 
+                        focus:outline-none 
+                        focus:ring-blue-300 
+                        dark:focus:ring-blue-800 
+                        font-bold 
+                        rounded-lg 
+                        text-base 
+                        px-5 
+                        py-2.5 
+                        text-center 
+                    ">
+                    {"Custom GPTs"}
+                    </a>
+                </div>
             </div>
         </div>
     }
