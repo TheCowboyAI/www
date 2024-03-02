67ef2438926e15687424e656b224f7722b7a12e5
Dave Kopi <david@thecowboy.ai>
2024-03-02 11:14:37 -0700

    removed bottom of landing

--- src/landing.rs
+++ src/landing.rs
@@ -34,7 +34,8 @@ pub fn Landing() -> impl IntoView {
                             {"Superior Data and Business Intelligence"}
                         </a>
                         <p class="text-black text-sm">
-                            {"For Projections, Predictions and Intelligent Decision Making"}
+                            {"For Projections, Predictions and Intelligent 
+                              Decision Making"}
                         </p>
                     </li>
                     <li class="mt-3 font-bold text-black text-xl md:text-2xl">
@@ -42,87 +43,19 @@ pub fn Landing() -> impl IntoView {
                             {"Cost Efficient Hosting"}
                         </a>
                         <p class="text-black text-sm">
-                            {"For the increased costs associated with the use of AI"}
+                            {"For the increased costs associated with 
+                              the use of AI"}
                         </p>
                     </li>
                 </ul>
+                
                 <div>
-                <h2 class="mt-10 p-3 font-bold text-white text-4xl md:text-6xl">
-                    {"Cowboy AI: Capture your Data"}
-                </h2>
-                <ul class="mt-3 text-black text-2xl md:text-3xl py-2">
-                    <li class="font-bold text-black text-xl md:text-2xl">
-                        <a href="/modeledintelligence">
-                            {"Unleash your True Data's Potential"}
-                        </a>
-                        <p class="text-black text-sm">
-                            {"Begin with a deep dive into your business DNA for tailored AI Solutions"}
-                        </p>
-                    </li>
-                    <li class="mt-3 font-bold text-black text-xl md:text-2xl">
-                        <a href="/workflow">
-                            {"Automate Intelligently"}
-                        </a>
-                        <p class="text-black text-sm">
-                            {"Inject our AI expertise to streamline and elevate your workflows"}
-                        </p>
-                    </li>
-                    <li class="mt-3 font-bold text-black text-xl md:text-2xl">
-                        <a href="/decisionmaking">
-                            {"Empower Decisions"}
-                        </a>
-                        <p class="text-black text-sm">
-                            {"Equip your strategy with our top-tier analytics and AI memory"}
-                        </p>
-                    </li>
-                    <li class="mt-3 font-bold text-black text-xl md:text-2xl">
-                        <a href="/predictions">
-                            {"Drive Results"}
-                        </a>
-                        <p class="text-black text-sm">
-                            {"Superior data for your AI means outpreforming the competition, every time."}
-                        </p>
-                    </li>
-                    <li class="mt-3 font-bold text-black text-xl md:text-2xl">
-                        <a href="/realtimemonitoring">
-                            {"Cost Efficient Cloud"}
-                        </a>
-                        <p class="text-black text-sm">
-                            {"Migrate to smarter hosting for leaner costs with Cowboy AI's unique infrastructure"}
-                        </p>
-                    </li>
-                    <li class="mt-3 font-bold text-black text-xl md:text-2xl">
-                        <a href="/gametheory">
-                            {"Optimized Performance"}
-                        </a>
-                        <p class="text-black text-sm">
-                            {"Our Cloud prowess fuels your AI, cutting expenses without compromise"}
-                        </p>
-                    </li>
-                    <li class="mt-3 font-bold text-black text-xl md:text-2xl">
-                        <a href="/">
-                            {"Command Excellence"}
-                        </a>
-                        <p class="text-black text-sm">
-                            {"Access Advanced AI tools that set your business operations apart"}
-                        </p>
-                    </li>
-                    <li class="mt-3 font-bold text-black text-xl md:text-2xl">
-                        <a href="/">
-                            {"Transform your Trajectory"}
-                        </a>
-                        <p class="text-black text-sm">
-                            {"Partner with Cowboy AI for an evolutionary leap in business intelligence"}
-                        </p>
-                    </li>
-                </ul>
-                </div>
-                <div>
-                    <h3 class="mt-10 p-3 font-bold text-red-500 text-4xl md:text-6xl">
+                    <h2 class="mt-10 p-3 font-bold text-red-500 text-4xl md:text-6xl">
                         {"Ransomware"}
-                    </h3>
+                    </h2>
                     <p class="mb-10">
-                    {"ASK US ABOUT our Proactive Ransomware Prevention and Protection"}
+                    {"ASK US ABOUT our Proactive Ransomware Prevention 
+                      and Protection"}
                     </p>
                 </div>
                 <div>
