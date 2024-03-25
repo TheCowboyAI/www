8f022ae28a5672d7ee64548d104d01ec3b7e3c93
Steele Price <steele@thecowboy.ai>
2024-03-24 20:11:13 -0700

    comments

--- src/cards.rs
+++ src/cards.rs
@@ -23,8 +23,8 @@ pub fn Card(
     <a href={route}>
       <img
         class="rounded-t-lg"
-        src={imgsrc} //"https://tecdn.b-cdn.net/img/new/standard/city/042.webp"
-        alt={imgalt} // "Palm Springs Road" />
+        src={imgsrc} 
+        alt={imgalt}
       />
     </a>
     <div class="p-6 shadow-lg">
@@ -39,47 +39,3 @@ pub fn Card(
   </div>
   }
 }
-
-
-// <div
-// class="mx-3 mt-6 flex flex-col rounded-lg bg-white text-surface shadow-secondary-1 dark:bg-surface-dark dark:text-white sm:shrink-0 sm:grow sm:basis-0">
-// <a href="#!">
-//   <img
-//     class="rounded-t-lg"
-//     src="https://tecdn.b-cdn.net/img/new/standard/city/044.webp"
-//     alt="Skyscrapers" />
-// </a>
-// <div class="p-6">
-//   <h5 class="mb-2 text-xl font-medium leading-tight">Card title</h5>
-//   <p class="mb-4 text-base">
-//     This is a wider card with supporting text below as a natural
-//     lead-in to additional content. This content is a little bit
-//     longer.
-//   </p>
-// </div>
-// <div
-//   class="mt-auto border-t-2 border-neutral-100 px-6 py-3 text-center text-surface/75 dark:border-white/10 dark:text-neutral-300">
-//   <small>Last updated 3 mins ago</small>
-// </div>
-// </div>
-
-// <div
-// class="mx-3 mt-6 flex flex-col rounded-lg bg-white text-surface shadow-secondary-1 dark:bg-surface-dark dark:text-white sm:shrink-0 sm:grow sm:basis-0">
-// <a href="#!">
-//   <img
-//     class="rounded-t-lg"
-//     src="https://tecdn.b-cdn.net/img/new/standard/city/043.webp"
-//     alt="Los Angeles Skyscrapers" />
-// </a>
-// <div class="p-6">
-//   <h5 class="mb-2 text-xl font-medium leading-tight">Card title</h5>
-//   <p class="mb-4 text-base">
-//     This card has supporting text below as a natural lead-in to
-//     additional content.
-//   </p>
-// </div>
-// <div
-//   class="mt-auto border-t-2 border-neutral-100 px-6 py-3 text-center text-surface/75 dark:border-white/10 dark:text-neutral-300">
-//   <small>Last updated 3 mins ago</small>
-// </div>
-// </div>
