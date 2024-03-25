fd743d3c5adfd1e23f10cbd67a9a29b5cb96da2c
Steele Price <steele@thecowboy.ai>
2024-03-24 19:25:32 -0700

    rephrase

--- src/cardtest.rs
+++ src/cardtest.rs
@@ -11,7 +11,7 @@ pub fn CardTest() -> impl IntoView {
         imgsrc="assets/CIM-Card.png".to_string() 
         imgalt="CIM".to_string()
         title="Composable Information Machine".to_string()
-        text="CIM is a systemized graph of information, every node and edge has a single purpose, designed for composition. We are transitioning from the world of Location-based Hierarchical Files, Rows and Columns, to something else.".to_string()
+        text="CIM is a new way to see the world of information. We are transitioning from the world of Location-based Hierarchical Files, Rows and Columns, to something else.".to_string()
         footer="Model your success".to_string()
       />
       <Card
