0b9673339828992387695cae86ceba8e9b7cfc40
Steele Price <steele@thecowboy.ai>
2024-03-25 18:35:52 -0700

    fix gpt list

--- src/ourgpts.rs
+++ src/ourgpts.rs
@@ -13,44 +13,8 @@ pub fn OurGPTs() -> impl IntoView {
                 <h3 class="mt-5 text-center text-2xl md:text-3xl lg:text-4xl font-bold">
                 {"Library of Agents"}
                 </h3>
-                <ul class="
-                    mt-10 px-5 py-2
-                    text-black text-base md:text-xl lg:text-2xl 
-                    "
-                >
-                    <li class="text-white text-xl md:text-2xl lg:text-4xl font-bold">
-                    {"Summarize2Md"}
-                    </li>
-                    <li class="ml-2">{"Upload a document, get a summary back in markdown"}</li>
-                    
-                    <li class="text-white text-xl md:text-2xl lg:text-4xl font-bold">
-                    {"Business Model Canvas Assistant"}
-                    </li>
-                    <li class="ml-2">{"Understand your model"}</li>
-                    <li class="ml-2">{"Generate a BMC"}</li>
-                    <li class="ml-2">{"Step-by-step Instruction"}</li>
-                    <li class="ml-2">{"Produce a persistant model"}</li>
-                    
-                    <li class="text-white text-xl md:text-2xl lg:text-4xl font-bold">
-                    {"Ontology Architect"}
-                    </li>
-                    <li class="ml-2">{"Create Taxonomies, Ontologies and Knowledge Graphs"}</li>
-                    
-                    <li class="text-white text-xl md:text-2xl lg:text-4xl font-bold">{"Event Sourcing Assistant"}</li>
-                    <li class="ml-2">{"Learn, Create and Use Event Sourced Systems"}</li>
-
-                    <li class="text-white text-xl md:text-2xl lg:text-4xl font-bold">{"Leptos Assistant"}</li>
-                    <li class="ml-2">{"Build systems in Leptos"}</li>
-                    <li class="ml-2">{"A cutting-edge Rust web framework"}</li>
-                    
-                    <li class="text-white text-xl md:text-2xl lg:text-4xl font-bold">{"NixOS Navigator"}</li>
-                    <li class="ml-2">{"Build determinate systems"}</li>
-                    <li class="ml-2">{"Deploy them anywhere"}</li>
-                    
-                    <li class="mt-10">
-                        <GptAccessForm />
-                    </li>
-                </ul>                
+                <img src="assets/gpts.png" class="mt-5 rounded-lg shadow-lg shadow-black" />
+                <GptAccessForm />
             </div>
         </div>
     }
