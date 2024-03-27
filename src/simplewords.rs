1064124f3d135bf80b3aa4129c596d1d4967042e
Steele Price <steele@thecowboy.ai>
2024-03-26 17:09:45 -0700

    styles

--- src/simplewords.rs
+++ src/simplewords.rs
@@ -3,52 +3,42 @@ use leptos::*;
 #[component]
 pub fn SimpleWords() -> impl IntoView {
     view! {
-    <div class="mt-10 flex items-center animate-fadeIn">
-      <div class="flex-row"> 
-          <h1 class="font-bold text-white text-4xl md:text-6xl">
+        <div class="mt-5 animate-fadeIn">
+        <div class="flex flex-col theme_bg-1-bg-3 gap-4 rounded-xl p-3 shadow-xl shadow-black">
+            <h1 class="font-bold rounded-lg shadow shadow-black p-2 text-center theme_bg-1-bg-2 theme_bg-1-color-5 text-xl" id="architecture">
               {"Evolution from Simple Words to Knowledge Graphs"}
           </h1>
-          <p class="mt-5">
+          <p>
               {"Simple words, when structured and related systematically, can evolve into complex systems of knowledge. This evolution typically progresses from a taxonomy, to an ontology, and finally to a knowledge graph."}
           </p>
-          <ol
-              class=" 
-                  mr-5 py-2
-                  text-black text-xl md:text-2xl lg:text-3xl 
-              "
-          >
-              <li class="mt-5">                    
-                  <details class="text-black text-sm">
-                      <summary class="text-white text-base font-bold">
-                          {"Taxonomy"}
-                      </summary>
-                      <p>
-                          {"A taxonomy is a hierarchical classification or framework that organizes concepts or words into groups or categories based on their similarities or relationships. It simplifies the understanding and retrieval of information by categorizing individual elements into a structured form."}
-                      </p>
-                  </details>
-              </li>
-              <li class="mt-5">                    
-                  <details class="text-black text-sm">
-                      <summary class="text-white text-base font-bold">
-                          {"Ontology"}
-                      </summary>
-                      <p>
-                          {"An ontology is a more advanced form of classification. It not only groups similar concepts but also defines the relationships between different concepts, providing a richer representation of knowledge by describing the nature of the relationships between those concepts."}
-                      </p>
-                  </details>
-              </li>
-              <li class="mt-5">                    
-                  <details class="text-black text-sm">
-                      <summary class="text-white text-base font-bold">
-                          {"Knowledge Graph"}
-                      </summary>
-                      <p>
-                          {"A knowledge graph is a network of interconnected entities and their interrelations. Knowledge graphs are used to integrate information from different sources and represent them in a way that is understandable and usable for advanced analysis and decision-making."}
-                      </p>
-                  </details>
-              </li>
-          </ol>
-          <p class="mt-5">
+        <details class="text-black text-sm">
+            <summary class="theme_bg-1-color-5 text-base font-bold">
+                {"Taxonomy"}
+            </summary>
+            <p>
+                {"A taxonomy is a hierarchical classification or framework that organizes concepts or words into groups or categories based on their similarities or relationships. It simplifies the understanding and retrieval of information by categorizing individual elements into a structured form."}
+            </p>
+        </details>
+
+        <details class="text-black text-sm">
+            <summary class="theme_bg-1-color-5 text-base font-bold">
+                {"Ontology"}
+            </summary>
+            <p>
+                {"An ontology is a more advanced form of classification. It not only groups similar concepts but also defines the relationships between different concepts, providing a richer representation of knowledge by describing the nature of the relationships between those concepts."}
+            </p>
+        </details>
+
+        <details class="text-black text-sm">
+            <summary class="theme_bg-1-color-5 text-base font-bold">
+                {"Knowledge Graph"}
+            </summary>
+            <p>
+                {"A knowledge graph is a network of interconnected entities and their interrelations. Knowledge graphs are used to integrate information from different sources and represent them in a way that is understandable and usable for advanced analysis and decision-making."}
+            </p>
+        </details>
+
+        <p class="">
               {"In essence, this progression represents the evolution of data into knowledge, enabling deeper insights, more informed decision-making, and more intelligent systems."}
           </p>
       </div>
--- src/tooling.rs
+++ src/tooling.rs
@@ -3,34 +3,28 @@ use leptos::*;
 #[component]
 pub fn Tooling() -> impl IntoView {
     view! {
-        <div class="mt-10 flex flex-col items-center animate-fadeIn">
-            <div class=""> 
-                <h1 class="font-bold text-white text-4xl md:text-6xl">
+        <div class="mt-5 animate-fadeIn">
+        <div class="flex flex-col theme_bg-1-bg-3 gap-4 rounded-xl p-3 shadow-xl shadow-black">
+            <h1 class="font-bold rounded-lg shadow shadow-black p-2 text-center theme_bg-1-bg-2 theme_bg-1-color-5 text-xl" id="architecture">
                 <a href="/modeledintelligence">
                 {"Modeled Intelligence"}
                 </a>
                 </h1>
-                <div class="text-black flex-row mt-10">
-                    <ul class="text-2xl">
-                    <li><a href="/businessmodel">{"Business model"}</a></li>
-                    <li>{"+"}</li>
-                    <li class="pb-2"><a href="/simplewords">{"simple words"}</a></li>
-                    </ul>
+                <div class="text-xl text-black flex flex-col mt-10">
+                    <a href="/businessmodel">{"Business model"}</a>
+                    <span>{"+"}</span>
+                    <a href="/simplewords">{"simple words"}</a>
                     <hr class="border border-t-2 border-black" />
                     <h2 class="text-2xl mt-2 font-bold italic">
-                    <a href="/aicomposable">{"AI Composable Insights"}</a>
+                        <a href="/aicomposable">{"AI Composable Insights"}</a>
                     </h2>
                 </div>
-                <a
-                  href="/gpts"
-                >
-                    <h2 class="mt-10 font-bold text-white text-3xl md:text-4xl">
-                    {"Custom GPTs"}
-                    <br />
-                    {"Fit Your Model"}
-                    </h2>
-                </a>
-            </div>
-        </div>
+            <a href="/gpts">
+              <h2 class="text-center p-2 rounded-lg theme_bg-1-bg-2 mt-10 font-bold theme_bg-1-color-5 text-xl">
+              {"Custom GPTs to fit your Model"}
+              </h2>
+          </a>
+          </div>
+          </div>
     }
 }
